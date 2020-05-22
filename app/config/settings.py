641412ac7dd28d171d92a4c1259996d5138ee827
moonpeter <moonpeter@kakao.com>
2020-05-22 17:24:20 +0900

    django-allauth 설치 및 설정

--- app/config/settings.py
+++ app/config/settings.py
@@ -72,6 +72,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'storages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.naver',
 ]
 
 MIDDLEWARE = [
@@ -153,3 +158,11 @@ USE_L10N = True
 USE_TZ = True
 
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
