4e8edfc47c4e0c9755b3019b4e111a2721b12396
moonpeter <moonpeter@kakao.com>
2020-05-25 13:59:47 +0900

    shop : 제품 상세페이지가 동작하도록 수정

--- app/shop/views.py
+++ app/shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from cart.forms import AddProductForm
 from shop.models import Category, Product
 
 
@@ -17,7 +18,13 @@ def product_in_category(request, category_slug=None):
                   {'current_category': current_category, 'categories': categories, 'products': products})
 
 
-def product_detail(reqeust, id, product_slug=None):
+def product_detail(request, id, product_slug=None):
     product = get_object_or_404(Product, id=id, slug=product_slug)
 
-    return render(reqeust, 'shop/detail.html', {'product': product})
+    return render(request, 'shop/detail.html', {'product': product})
+
+
+def product_detail(request, id, product_slug=None):
+    product = get_object_or_404(Product, id=id, slug=product_slug)
+    add_to_cart = AddProductForm(initial={'quantity': 1})
+    return render(request, 'shop/detail.html', {'product': product, 'add_to_cart': add_to_cart})
