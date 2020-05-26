cf38d57cbaea6938eb9b108be79f072f00c19fda
moonpeter <moonpeter@kakao.com>
2020-05-26 10:22:36 +0900

    order : OrderItem models 작성

--- app/order/models.py
+++ app/order/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 from coupon.models import Coupon
+from shop.models import Product
 
 
 class Order(models.Model):
@@ -31,4 +32,16 @@ class Order(models.Model):
     def get_total_price(self):
         total_product = self.get_total_product()
         return total_product - self.discount
-    
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='order_products')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_item_price(self):
+        return self.price * self.quantity
