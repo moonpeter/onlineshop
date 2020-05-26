55941566a1fc5b7472619c25f422f0a0f12cb9d6
moonpeter <moonpeter@kakao.com>
2020-05-26 11:48:37 +0900

    STATICFILES_DIRS 설정

--- app/config/settings.py
+++ app/config/settings.py
@@ -52,6 +52,7 @@ AWS_LOCATION = 'static'
 STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
